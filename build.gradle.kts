60fb711757382c667296254ed7063a306837cfb9
Oliver Kopp <kopp.dev@gmail.com>
2025-06-10 13:22:33 +0200

    Use testImplementation (#1)

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,12 +14,12 @@ dependencies {
 
     // wiremock patch
     testImplementation("org.wiremock:wiremock:3.13.0")
-    implementation("com.github.koppor:wiremock-slf4j-shim:main-SNAPSHOT")
+    testImplementation("com.github.koppor:wiremock-slf4j-shim:main-SNAPSHOT")
     testImplementation("com.github.koppor:wiremock-slf4j-spi-shim:main-SNAPSHOT")
-    implementation("com.github.jknack:handlebars:4.3.1") {
+    testImplementation("com.github.jknack:handlebars:4.3.1") {
         exclude(group = "org.mozilla", module = "rhino")
     }
-    implementation("com.github.jknack:handlebars-helpers:4.3.1") {
+    testImplementation("com.github.jknack:handlebars-helpers:4.3.1") {
         exclude(group = "org.mozilla", module = "rhino")
         exclude(group = "org.apache.commons", module = "commons-lang3")
     }
